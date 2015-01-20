6d019f392a3d7a18d286e1fc679f369921cb0844
Derek Adair <d@derekadair.com>
2015-01-20 17:11:24 -0600

    fixed django settings for django_debug_toolbar, no longer including internal IPs in dev.py

--- django_app/settings/base.py
+++ django_app/settings/base.py
@@ -145,12 +145,11 @@ INSTALLED_APPS = (
 
     #'api',
     'core',
+    'web',
     'flexgallery',
 )
 
 EMAIL_SUBJECT_PREFIX = '[subject] '
-
-INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
 
 # django-debug-toolbar settings
 DEBUG_TOOLBAR_CONFIG = {
--- django_app/settings/dev.py
+++ django_app/settings/dev.py
@@ -1,7 +1,6 @@
 from .base import *
 
 DEBUG = True
-INTERNAL_IPS = ('50.134.190.135')
 try:
 	from .local import *
 except ImportError:
