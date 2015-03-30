9f0b1e7f802fb5f1b131d2b099a26e89a2ae8a88
Derek Adair <d@derekadair.com>
2015-03-30 16:28:54 -0500

    Including media urls for dev

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
@@ -16,4 +17,4 @@ urlpatterns = patterns('',
     url(r'^api/', include('flex_api.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'', include('web.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
